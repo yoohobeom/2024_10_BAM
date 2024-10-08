0f904f4842e2f9b6b65ecf54c0914191728cdd57
yoohobeom <ghqja25@gmail.com>
2024-10-08 15:18:12 +0900

    게시판 틀잡기

---
+++ src/com/koreait/BAM/Main.java
@@ -0,0 +1,27 @@
+package com.koreait.BAM;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("== 프로그램 시작 ==");
+
+		Scanner sc = new Scanner(System.in);
+
+		while (true) {
+			System.out.printf("명령어) ");
+			
+			String cmd = sc.nextLine();
+
+			
+
+			if (cmd.equals("exit")) {
+				break;
+			}
+		}
+		
+		sc.close();
+		
+		System.out.println("== 프로그램 종료 ==");
+	}
+}
